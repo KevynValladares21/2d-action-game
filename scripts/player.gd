9f3adf2a0cb266c454376ab40400242393604c10
Kevyn Valladares <kevyn.valladares21@gmail.com>
2025-06-13 19:29:14 -0600

    Smoothed animations and collider interactions

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
-
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+var is_attacking = false
+var attack_switch = false
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var sprite_2d: Sprite2D = $SpritePivot/Sprite2D
+@onready var sprite_pivot: Node2D = $SpritePivot
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,34 +15,57 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor() and not is_attacking:
 		velocity.y = JUMP_VELOCITY
-
+	
+	_handle_attacking()
+	_handle_movement_and_animation()
+	
+	move_and_slide()
+	
+func _handle_attacking():
+	# Handle Attacking
+	if Input.is_action_just_pressed("primary-attack") and not is_attacking:
+		is_attacking = true
+		if attack_switch:
+			attack_switch = false
+			animation_player.play("Attack2")
+		else:
+			attack_switch = true
+			animation_player.play("Attack1")
+	
+func _handle_movement_and_animation():
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
 	
-	# Flip the sprite
-	if direction > 0:
-		sprite_2d.flip_h = false
-	elif direction < 0:
-		sprite_2d.flip_h = true
+	if not is_attacking:
+		# Flip the sprite
+		if direction > 0:
+			sprite_pivot.scale.x = 1
+		elif direction < 0:
+			sprite_pivot.scale.x = -1
 	
-	# Play animations
-	if is_on_floor():
-		if direction == 0:
-			animation_player.play("Idle")
+	# Play animations and handle movements
+	if not is_attacking:
+		if is_on_floor():
+			if direction == 0:
+				animation_player.play("Idle")
+			else:
+				animation_player.play("Run")
 		else:
-			animation_player.play("Run")
-	else:
-		if (velocity.y < 0):
-			animation_player.play("Jump")
+			if (velocity.y < 0):
+				animation_player.play("Jump")
+			else:
+				animation_player.play("Fall")
+		
+		# Handle Horizontal movements
+		if direction:
+			velocity.x = direction * SPEED
 		else:
-			animation_player.play("Fall")
-			
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+	elif is_on_floor():
+		velocity.x = 0
 
-	move_and_slide()
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "Attack1" or anim_name == "Attack2":
+		is_attacking = false
